45b7b24c6034bd56ceab2ffb35fda892649e67b0
Nivin s <nivinsvysakh2023@gmail.com>
2023-05-15 16:55:02 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    int a , b ;
+    printf("Enter you 1  Number :");
+    scanf("%d",&a);
+    printf("Enter your 2 Number");
+    scanf("%d" ,&b);
+
+    if (a > b)
+    {
+        printf("A is Bigger than B");
+    }
+    else{
+        printf("B is Bigger than A");
+    }
+    
+
+
+    return 0;
+}
+
